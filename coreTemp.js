a5e1ca09239344c6ed313e66ba4aba8a9e20f689
NadirMansur <nadirmansur89@gmail.com>
2023-03-31 18:04:45 -0300

    se HIZO MAS legibe el coretemp

--- coreTemp.js
+++ coreTemp.js
@@ -3,10 +3,12 @@ const { exec } = require('child_process');
 
 // Ejecutar el comando "sensors" en la terminal
 exec('sensors', (error, stdout, stderr) => {
+console.log("////////////////////////////////")
   if (error) {
     console.error(`Error al ejecutar el comando: ${error}`);
     return;
   }
   console.log(`stdout: ${stdout}`);
   console.error(`stderr: ${stderr}`);
+  console.log("////////////////////////////////")
 });
